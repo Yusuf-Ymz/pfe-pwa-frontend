1aaacac0c3fc23b3b2b4c618534f4ffff711d6fa
YYIPL <yyusuf.ipl@gmail.com>
2020-12-03 19:34:46 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 //Install express server
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/pfe-web-frontend'));
+app.use(express.static(__dirname + "/dist/pfe-pwa-frontend"));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/pfe-web-frontend/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/pfe-pwa-frontend/index.html"));
 });
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

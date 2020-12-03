30c82dde32f70469910d11e6f83da229fa9099e1
YYIPL <yyusuf.ipl@gmail.com>
2020-12-03 23:17:47 +0100

    modification package.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,4 @@
-const environment = { production: false, serverUrl: "http://localhost:8080" };
-if (process.env.ENV === "dev") {
-  environment.serverUrl = 'https://pfe-api-backend-dev.herokuapp.com/'
-}
-else if (process.env.ENV === "production") {
-  environment.production = true;
-  environment.serverUrl = 'https://pfe-api-backend.herokuapp.com/'
-}
-export { environment };
+export const environment = {
+  production: false,
+  serverUrl: 'http://localhost:8080/'
+};

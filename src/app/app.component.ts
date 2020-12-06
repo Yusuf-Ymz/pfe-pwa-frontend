cfdfe96f966d35e5050e24e0b3b64f8f6853c0a2
YYIPL <yyusuf.ipl@gmail.com>
2020-12-06 14:42:18 +0100

    premier test du scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { OnInit, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('scanner')
+  scanner: ZXingScannerComponent;
+
   title = 'pfe-pwa-frontend';
+
+  activated: boolean = false;
+
+  qrResultString: string;
+
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCodeResult(resultString: string) {
+    this.activated = false;
+    this.qrResultString = resultString;
+    this.scanner.enable = false;
+  }
+
+  activate() {
+    this.activated = true;
+  }
+
+  deactivate() {
+    this.activated = false;
+    this.scanner.enable = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ZXingScannerModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
